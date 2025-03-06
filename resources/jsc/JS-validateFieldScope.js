eb7415da3323943b7b473d6ccb65faf2d5f836a9
lekkalahyma <lekkalahyma2001@gmail.com>
2025-03-06 11:53:26 +0530

    addedthecode

--- resources/jsc/JS-validateFieldScope.js
+++
@@ -1,9 +0,0 @@
-var fieldScope = context.getVariable("requestLogs.fieldScope");
-var appScopes = context.getVariable('verifyapikey.Verify-API-Key.app_scope');
-appScopes = appScopes ? appScopes.split(',') : '';
-var isValidFieldScope = appScopes.indexOf(fieldScope) == -1 ? false : true;
-if(isValidFieldScope === false)
-{
-    context.setVariable("ErrorMessage","Scope Validation Failed");
-    throw 'Scope Validation Failed';
-}
